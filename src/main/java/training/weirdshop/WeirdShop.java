8fc384266805855d84bd072ef1cbab53a7ca0f2c
Amarpal.Amrith <amarpal.amrith@AmarpalAmriths-MacBook-Pro.local>
2019-03-28 12:33:48 +0000

    Refactored

--- src/main/java/training/weirdshop/WeirdShop.java
+++ src/main/java/training/weirdshop/WeirdShop.java
@@ -9,54 +9,80 @@ class WeirdShop {
 
     void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage Pass")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Gold Coin")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+
+            sellInIncrement(items[i]);
+            updateQuality(items[i]);
+            checkQualityIsInBounds(items[i]);
+            
+        }
+    }
+
+    public static boolean isAgedBrie(Item item) {
+        return item.name.equals("Aged Brie");
+    }
+
+    public static boolean isBackStagePass(Item item) {
+        return item.name.equals("Backstage Pass");
+    }
+
+
+    public static boolean isGoldCoin(Item item) {
+        return item.name.equals("Gold Coin");
+    }
+
+    private void agedBrieQuality(Item item) {
+        if (isAgedBrie(item) && item.quality < 50) {
+            item.quality++;
+        }
+    }
+
+    private void backStagePassQuality(Item item) {
+        if (isBackStagePass(item)) {
+            if (item.sellIn >= 0) {
+                if (item.sellIn > 10) {
+                    item.quality++;
+                } else if (item.sellIn > 5) {
+                    item.quality = item.quality + 2;
+                } else {
+                    item.quality = item.quality + 3;
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage Pass")) {
-                        if (items[i].sellIn < 12) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 7) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+                item.quality = 0;
             }
+        }
 
-            if (!items[i].name.equals("Gold Coin")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+    }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage Pass")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Gold Coin")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+    private void goldCoinQuality(Item item) {
+        if (isGoldCoin(item)) {
+            item.quality = 80;
+        }
+    }
+
+    private void otherItemsQuality(Item item) {
+        if (!isAgedBrie(item) && !isBackStagePass(item) && !isGoldCoin(item) && item.quality > 0) {
+            item.quality--;
+            if (item.sellIn < 0) {
+                item.quality--;
             }
+        }
+    }
+    private void sellInIncrement(Item item) {
+        if (!isGoldCoin(item)) {
+            item.sellIn--;
+        }
+    }
+    private void updateQuality(Item item) {
+        agedBrieQuality(item);
+        backStagePassQuality(item);
+        goldCoinQuality(item);
+        otherItemsQuality(item);
+    }
+    private void checkQualityIsInBounds(Item item) {
+        if (item.quality > 50 && !isGoldCoin(item)) {
+            item.quality = 50;
+        } else if (item.quality < 0) {
+            item.quality = 0;
         }
     }
 }
