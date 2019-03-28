0271e957a3aa4ec7f2b5735d2a79f6ad7a19fffc
Amarpal.Amrith <amarpal.amrith@AmarpalAmriths-MacBook-Pro.local>
2019-03-28 15:09:39 +0000

    Premium items added

--- src/main/java/training/weirdshop/WeirdShop.java
+++ src/main/java/training/weirdshop/WeirdShop.java
@@ -7,13 +7,14 @@ class WeirdShop {
         this.items = items;
     }
 
-    void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
+    void updateQuality() { //MAIN
+        for (Item item : items) {
 
-            sellInIncrement(items[i]);
-            updateQuality(items[i]);
-            checkQualityIsInBounds(items[i]);
-            
+            item.sellIn += sellInIncrement(item);
+            updateQuality(item);
+            updatePremiumQuality(item);
+            checkQualityIsInBounds(item);
+
         }
     }
 
@@ -30,59 +31,106 @@ class WeirdShop {
         return item.name.equals("Gold Coin");
     }
 
-    private void agedBrieQuality(Item item) {
-        if (isAgedBrie(item) && item.quality < 50) {
-            item.quality++;
+    private int agedBrieQuality(Item item) {
+        if (item.quality < 50) {
+            return 1;
         }
+        return 0;
     }
 
-    private void backStagePassQuality(Item item) {
-        if (isBackStagePass(item)) {
-            if (item.sellIn >= 0) {
-                if (item.sellIn > 10) {
-                    item.quality++;
-                } else if (item.sellIn > 5) {
-                    item.quality = item.quality + 2;
-                } else {
-                    item.quality = item.quality + 3;
-                }
+    private int backStagePassQuality(Item item) {
+        if (item.sellIn >= 0) {
+            if (item.sellIn > 10) {
+                return 1;
+            } else if (item.sellIn > 5) {
+                return 2;
             } else {
-                item.quality = 0;
+                return 3;
             }
-        }
-
-    }
-
-    private void goldCoinQuality(Item item) {
-        if (isGoldCoin(item)) {
-            item.quality = 80;
+        } else {
+            return -item.quality;
         }
     }
 
-    private void otherItemsQuality(Item item) {
-        if (!isAgedBrie(item) && !isBackStagePass(item) && !isGoldCoin(item) && item.quality > 0) {
-            item.quality--;
+    private int goldCoinQuality(Item item) {
+        return 0;
+    }
+
+    private int otherItemsQuality(Item item) {
+        int total = 0;
+        if (item.quality > 0) {
+            total--;
             if (item.sellIn < 0) {
-                item.quality--;
+                total--;
             }
         }
+        return total;
     }
-    private void sellInIncrement(Item item) {
-        if (!isGoldCoin(item)) {
-            item.sellIn--;
+
+    private boolean isSomeOtherItem(Item item) {
+        return item.name.equals("Some other item");
+    }
+
+    private int sellInIncrement(Item item) {
+        if (!isGoldCoin(item) && !isPremiumGoldCoin(item)) {
+            return -1;
+        }
+        return 0;
+    }
+
+    private void updateQuality(Item item) {
+        if (isAgedBrie(item)) {
+            item.quality = item.quality + agedBrieQuality(item);
+        }
+        if (isBackStagePass(item)) {
+            item.quality = item.quality + backStagePassQuality(item);
+        }
+        if (isGoldCoin(item)) {
+            item.quality = item.quality + goldCoinQuality(item);
+        }
+        if (isSomeOtherItem(item)) {
+            item.quality = item.quality + otherItemsQuality(item);
         }
     }
-    private void updateQuality(Item item) {
-        agedBrieQuality(item);
-        backStagePassQuality(item);
-        goldCoinQuality(item);
-        otherItemsQuality(item);
-    }
+
     private void checkQualityIsInBounds(Item item) {
-        if (item.quality > 50 && !isGoldCoin(item)) {
+        if (item.quality > 50 && !isGoldCoin(item) && !isPremiumGoldCoin(item)) {
             item.quality = 50;
         } else if (item.quality < 0) {
             item.quality = 0;
         }
+    }
+
+    ////////////////////////////////PREMIUM STUFF////////////////////////////////////////////////
+    private void updatePremiumQuality(Item item) {
+        if (isPremiumAgedBrie(item)) {
+            item.quality = item.quality + (agedBrieQuality(item) * 2);
+        }
+        if (isPremiumBackStagePass(item)) {
+            item.quality = item.quality + (backStagePassQuality(item) * 2);
+        }
+        if (isPremiumGoldCoin(item)) {
+            item.quality = item.quality + (goldCoinQuality(item) * 2);
+        }
+        if (isSomeOtherPremiumItem(item)) {
+            item.quality = item.quality + (otherItemsQuality(item) * 2);
+        }
+    }
+
+    public static boolean isPremiumAgedBrie(Item item) {
+        return item.name.equals("Premium Aged Brie");
+    }
+
+    public static boolean isPremiumBackStagePass(Item item) {
+        return item.name.equals("Premium Backstage Pass");
+    }
+
+
+    public static boolean isPremiumGoldCoin(Item item) {
+        return item.name.equals("Premium Gold Coin");
+    }
+
+    public static boolean isSomeOtherPremiumItem(Item item) {
+        return item.name.equals("Some other Premium item");
     }
 }
